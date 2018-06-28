362eb599ca622f86010672263e7e0916577ca97f
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2018-06-28 12:45:31 +0200

    chore: Update webpack config to work with webpack 4

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const env = process.env.NODE_ENV;
 const production = env === 'production';
@@ -13,30 +12,34 @@ const production = env === 'production';
 const page = (name) => {
   return new HtmlWebpackPlugin({
     inject: true,
-    template: path.join(__dirname, `/../src/pug/${name}.pug`),
-    filename: path.join(__dirname, `/../dev/${name}.html`)
+    template: path.join(__dirname, `../src/pug/${name}.pug`),
+    filename: path.join(__dirname, `../dev/${name}.html`)
   });
 };
 
 const config = {
   devtool: production ? 'source-map' : 'cheap-source-map',
   entry: {
-    app: path.join(__dirname, '/../src/js/app.js')
+    app: path.join(__dirname, '../src/js/app.js')
   },
   output: {
-    path: path.join(__dirname, '/../dev/dist'),
+    path: path.join(__dirname, '../dev/dist'),
     filename: 'js/[name].js',
     publicPath: 'dist/'
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor' }),
+    new webpack.LoaderOptionsPlugin({ options: {} }),
     new FriendlyErrorsWebpackPlugin(),
     new ProgressBarPlugin(),
-    new ExtractTextPlugin({ filename: 'css/app.css' }),
     new CopyWebpackPlugin([{ from: path.join(__dirname, '/../src/img'), to: '../img/' }]),
     page('index'),
     page('render')
   ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
   watchOptions: {
     aggregateTimeout: 300,
     poll: 1000
@@ -52,58 +55,54 @@ const config = {
   },
 
   module: {
-    rules: [{
-      test: /.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: { babelrc: true }
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { babelrc: true }
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.styl$/,
+        loader: ['style-loader', 'css-loader', 'stylus-loader?resolve url']
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          name: 'font/[name].[ext]'
+        }
+      },
+      {
+        test: /.pug$/,
+        exclude: /node_modules/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
       }
-    },
-    {
-      test: /.js$/,
-      exclude: /node_modules/,
-      loader: 'eslint-loader',
-      enforce: 'pre'
-    },
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    },
-    {
-      test: /\.vue$/,
-      loader: 'eslint-loader',
-      enforce: 'pre'
-    },
-    {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: 'css-loader'
-      })
-    },
-    {
-      test: /.styl$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'stylus-loader?resolve url']
-      })
-    },
-    {
-      test: /\.(ttf|eot|svg)(\?.*)?$/,
-      loader: 'file-loader',
-      options: {
-        name: 'fonts/[name].[ext]'
-      }
-    },
-    {
-      test: /.pug$/,
-      exclude: /node_modules/,
-      loader: 'pug-loader',
-      options: {
-        pretty: true
-      }
-    }]
+    ]
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
