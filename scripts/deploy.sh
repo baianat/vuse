bedb0994fdba0ef96655d5a1d4b84763da9d2318
Abdelrahman Ismail <abdelrahman3d@gmail.com>
2018-12-03 14:55:26 +0200

    chore: add docs deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run docs:build
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:baianat/vues.git master:gh-pages
+
+cd -
